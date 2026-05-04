4699ed3c04949fc3a23b5fff978decb0e53185d9
ConnorFSA <connor.d.freebairn04@gmail.com>
2026-05-04 19:58:09 +0930

    Fixes to batabase reading in prod

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,8 +1,17 @@
 import sqlite3
+import os
 from flask import g
 
-DATABASE = "../../db/projects.db"
+# get the absolute path of this file then go up three levels to get the base directory of the project
+BASE_DIR = os.path.dirname(  # /home/ubuntu/Web_Portfolio
+    os.path.dirname(          # /home/ubuntu/Web_Portfolio/backend
+        os.path.dirname(      # /home/ubuntu/Web_Portfolio/backend/app
+            os.path.abspath(__file__)  # /home/ubuntu/Web_Portfolio/backend/app/db.py
+        )
+    )
+)
 
+DATABASE = os.path.join(BASE_DIR, 'database', 'portfolio.db')
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(DATABASE)
